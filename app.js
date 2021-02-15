b661c47a74fc12cfba716257263a92cdb68cbc78
Jorge Alfredo Alvarez Guzman <jorgealfredoalvarezguzman@Jorges-MacBook-Pro.local>
2021-02-15 14:48:57 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first pipeline on AWS!'));
+app.get('/', (req, res) => res.send('This is my first pipeline on AWS! With a change.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
